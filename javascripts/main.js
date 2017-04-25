d9c37a6dd1fe3154cc121bf6a399dadfbae45cbf
Meghan Debity <mdebity@gmail.com>
2017-04-24 21:35:34 -0500

    working api code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log("Javascript loading");
+$(document).ready(function(){
+	const apiKey = "";
+
+$("body").on("click", "li", (e) => {
+	// console.log(e.target.innerHTML);
+
+		loadPlaces(e.target.innerHTML)
+		.then((data) => {
+		console.log(data);
+		}).catch((error) => {
+		console.log(error);
+		});
+			
+	});
+
+//get list of names printing to DOM
+});
+
+	const loadPlaces = (dropdownType) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&radius=500&type=${dropdownType}&key=${apiKey}`)
+		.done((data) => resolve(data))
+		.fail((error) => reject(error));
+	});
+	};
+
+;
